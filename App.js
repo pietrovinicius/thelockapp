80d7a4e903b8cd2a1878675cea7c66d9147d0937
pietro lim a <pietro.lima@calindra.com.br>
2018-11-12 00:23:13 -0200

    SplashScreen e BackButton Android
    Adição da tela de splash e configuração do botão voltar no android...

--- App.js
+++ App.js
@@ -1,15 +1,124 @@
+import React, { Component } from 'react';
+import {
+  WebView,
+  Text,
+  View,
+  Image,
+  Alert,
+  ActivityIndicator,
+} from 'react-native';
+import { BackHandler } from 'react-native';
 
-import React, {Component} from 'react';
-import {WebView} from 'react-native';
+export default class App extends Component {
 
-type Props = {};
-export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timePassed: false,
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => { this.setState({ timePassed: true }) }, 2000);
+  }
+
+  componentWillMount() {
+    console.log("=================WillMount()");
+    BackHandler.addEventListener('hardwareBackPress', this.handleBackButton);
+  }
+
+  componentWillUnmount() {
+    console.log("=================WillUnMount()");
+    BackHandler.removeEventListener('hardwareBackPress', this.handleBackButton);
+  }
+
+  handleBackButton = () => {
+    console.log("=================Back()");
+    Alert.alert(
+      '',
+      'Deseja Sair do App?',
+      [
+        { text: 'Cancelar', onPress: () => console.log('=============Cancelar'), style: 'cancel' },
+        {
+          text: 'Sim', onPress: (() => {
+            BackHandler.exitApp();
+            console.log('=========Sim');
+          })
+        },
+      ],
+      { cancelable: false }
+    )
+    return true;
+  };
+
+  setTimePassed() {
+    this.setState({ timePassed: true });
+  }
+ displaySpinner() {
+  return (
+    <View style={styles.container}>
+      {console.log("=================Carregando WebView...")}      
+      <ActivityIndicator size="large" color="#000000" />
+      <Text>Carregando...</Text>
+    </View>
+  );
+}
   render() {
-    return (
-        <WebView
-            source={{uri: 'http://www.thelock.com.br/app/'}}
-            style={{position:'relative'}}
-        />
-    );
+    if (!this.state.timePassed) {
+      return (
+        <View style={styles.container}>
+        {console.log("=================Carregando Imagem...")}                 
+          <Image style={styles.Image}
+            source={require('./src/img/002.png')}
+            resizeMode='center'            
+          />
+        </View>
+      );
+    } else {
+        return (                 
+            <WebView              
+            source={{ uri: 'http://www.thelock.com.br/app/' }}
+              style={styles.WebView}
+              startInLoadingState={true}
+              renderLoading={() => {return this.displaySpinner()}}
+              onError={()=>{
+                Alert.alert(
+                  'Informação',
+                  'Para uso correto do aplicativo \né necessário conexão com a internet',
+                  [
+                    {
+                      text: 'Ok', onPress: (() => {
+                        BackHandler.exitApp();
+                        console.log('=========Sim');
+                      })
+                    },
+                  ],
+                  { cancelable: false }
+                )
+              }}
+            />
+        );
+    }
+  }
+}
+
+const styles = {
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  WebView: {
+    position: 'relative'
+  },
+  Image: {
+    resizeMode: 'center'
+  },
+  Texto: {
+    color: 'red',
+    fontSize: 30,
+  },
+  ActivityIndicator:{
+    paddingTop: 220,
   }
 }
